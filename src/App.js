01e82c56a07ee6d8e59bc61119de8dded0c13c04
Henrique Garcia <https.henriquegarcia@gmail.com>
2021-11-13 14:48:19 -0300

    Configuraçao inicial do arquivos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <Router>
+      <GlobalStyle />
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/globalStyles.js
@@ -0,0 +1,14 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: none;
+    outline: none;
+    font-family: 'Roboto', sans-serif;
+  }
+`
+
+export default GlobalStyle;
